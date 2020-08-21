f4c69cc5e66fe7945d1ea57b9e37a601a38bb506
aidarous <aidarous12@gmail.com>
2020-08-21 17:12:37 -0400

    edited header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Header from '../src/components/Header';
+import About from'../src/components/About';
+import Projects from'../src/components/Projects';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <Header /> <br /><br />
+        <About />
+        <Projects />
       
       </header>
     </div>
---
+++ src/components/about.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+    render() {
+        return (
+            <div>
+                <section id="about">
+        <div className="row">
+          <div className="three columns">
+            <img className="profile-pic" src="images/profilepic.jpg" alt="" />
+          </div>
+          <div className="nine columns main-col">
+            <h2>About Me</h2>
+            <p>
+            </p>
+            <div className="row">
+              <div className="columns contact-details">
+                
+              </div>
+              
+            </div> {/* end row */}
+          </div> {/* end .main-col */}
+        </div>
+      </section>
+            </div>
+        )
+    }
+}
