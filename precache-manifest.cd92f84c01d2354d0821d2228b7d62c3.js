973f44694142cae6ec715375b965eddcc88e4dbb
aidarous <aidarous12@gmail.com>
2020-10-23 16:36:49 -0400

    Updates

---
+++ precache-manifest.cd92f84c01d2354d0821d2228b7d62c3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c0b0e7a8cd82b63e3e4bba54bf5490b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "be5af05a0bd6e794bce1",
+    "url": "/portfolio/static/css/main.d254bb90.chunk.css"
+  },
+  {
+    "revision": "03d65cfe2ff6a7f131a0",
+    "url": "/portfolio/static/js/2.4e63a2bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.4e63a2bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be5af05a0bd6e794bce1",
+    "url": "/portfolio/static/js/main.028b4d35.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "8387ab188b623bf8df283bba141daf9e",
+    "url": "/portfolio/static/media/basketballrps.8387ab18.png"
+  },
+  {
+    "revision": "bd01fe7f0a7fb39161c12777029ac926",
+    "url": "/portfolio/static/media/bballrps.bd01fe7f.png"
+  },
+  {
+    "revision": "56401f222ddf8f8e9192acbbdaa48fcb",
+    "url": "/portfolio/static/media/profilepic.56401f22.png"
+  }
+]);
